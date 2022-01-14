9dc2a742cbfcf2f2d49d9ada332b5177e635e365
Varunda <trvarunda@gmail.com>
2022-01-13 22:06:57 -0800

    undo brug

--- Startup.cs
+++ Startup.cs
@@ -218,7 +218,7 @@ namespace watchtower {
 
             logger.LogInformation($"Environment: {env.EnvironmentName}");
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseStaticFiles();
             app.UseRouting();
