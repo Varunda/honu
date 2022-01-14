2c77b4927dc2106a9f3021c8b57a9d768d9493ea
Varunda <trvarunda@gmail.com>
2022-01-13 22:11:00 -0800

    aaaaaaaaaa

--- Startup.cs
+++ Startup.cs
@@ -202,15 +202,22 @@ namespace watchtower {
                 //services.AddHostedService<CharacterDatesFixerStartupService>();
                 //services.AddHostedService<OutfitMemberFixerStartupService>();
             }
+
+            services.Configure<ForwardedHeadersOptions>(options => {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                // Only loopback proxies are allowed by default.
+                // Clear that restriction because forwarders are enabled by explicit 
+                // configuration.
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env,
             IHostApplicationLifetime lifetime, ILogger<Startup> logger) {
 
-            app.UseForwardedHeaders(new ForwardedHeadersOptions {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto,
-            });
+            app.UseForwardedHeaders();
 
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
