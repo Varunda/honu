639c8e24233dc070ec5ef8dccde3bf29c4f436cb
Varunda <trvarunda@gmail.com>
2022-02-07 15:06:02 -0800

    remove hack cuz it didn't work

--- libraries/DaybreakGames.Census/src/DaybreakGames.Census/CensusOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DaybreakGames.Census
-{
-    public class CensusOptions
-    {
-        public string CensusServiceId { get; set; } = Constants.DefaultServiceId;
-        public string CensusServiceNamespace { get; set; } = Constants.DefaultServiceNamespace;
-        public bool LogCensusErrors { get; set; } = false;
-    }
-}
--- libraries/DaybreakGames.Census/src/DaybreakGames.Census/ICensusQueryFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using DaybreakGames.Census.Operators;
-
-namespace DaybreakGames.Census
-{
-    public interface ICensusQueryFactory
-    {
-        CensusQuery Create(string serviceName);
-    }
-}
