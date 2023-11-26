fe2c6818a22e9f9af856822343dbd405e0ae3d69
Varunda <trvarunda@gmail.com>
2023-11-25 22:25:45 -0800

    im tired

--- src/api/OutfitWarsOutfitApi.ts
+++ src/api/OutfitWarsOutfitApi.ts
@@ -52,6 +52,7 @@ export class OutfitWarsOutfitApi extends ApiWrapper<OutfitWarsOutfit> {
     public static parseFlat(elem: any): FlatOutfitWarsOutfit {
         return {
             ...elem.entry,
+            signupCount: (elem.status == "Full") ? 24 : elem.signupCount,
             outfit: elem.outfit != null ? OutfitApi.parse(elem.outfit) : null
         };
     }
